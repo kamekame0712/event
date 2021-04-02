d3115a6c5aed24277f5b5802776e8049ed70939a
kamei <s-kamei@chuoh-kyouiku.co.jp>
2021-04-02 18:04:21 +0900

    関西デザイン修正

--- app/views/front/summer21/kansai.php
+++ app/views/front/summer21/kansai.php
@@ -20,8 +20,8 @@
 
 				<div class="detail-box for-pc" id="detail_box">
 					<div class="container-fluid">
-						<div class="row">
-							<div class="col-12 col-md-6">
+						<div class="row align-items-end">
+							<div class="col-12 col-md-6 px-md-0 py-md-o">
 								<h3 class="speaker-name">安河内 哲也<span>氏</span></h3>
 								<p>
 									英語教育改革が進むなか、今の子どもたちに必要な英語とは何か。
@@ -39,51 +39,54 @@
 										12:15～12:30　質疑応答
 									</dd>
 								</dl>
+
+								<div class="limit-num" id="limit_num">
+									<img src="<?= base_url('img/summer21/limit.png') ?>" alt="１塾２名限定">
+
+									<h4>定員</h4>
+
+									<div class="place-limit">
+										<span class="place-name">大阪</span>会場
+										<span class="first-come">先着</span>
+										<span class="number">150</span>
+										<span class="first-come">名</span>
+									</div>
+
+									<div class="place-limit">
+										<span class="place-name">京都</span>会場
+										<span class="first-come">先着</span>
+										<span class="number">&nbsp;80</span>
+										<span class="first-come">名</span>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-12 col-md-6 px-md-0 py-md-0">
+								<div class="speaker-box" id=speaker_box>
+									<h3 class="seminar-title">今の子どもたちに<br>必要な英語とは</h3>
+									<div class="speaker-info">
+										<div class="container-fluid">
+											<div class="row">
+												<div class="col-8">
+													<div class="text-center">
+														<h4>講師紹介</h4>
+													</div>
+													<p>
+														1967年福岡県北九州市生まれ。上智大学外国語学部英語学科卒。
+														東進ハイスクール・東進ビジネススクール英語科講師。
+														言語活動型英語授業を促進するために、スピーキングテスト、４技能試験の導入にむけて各所で活動中。
+														学校での講演の他、大手メーカーや金融機関でのグローバル化研修、教育委員会主催の教員研修事業の講師も務めている。
+													</p>
+												</div>
+											</div> <!-- end of .row -->
+										</div> <!-- end of .container-fluid -->
+
+										<img src="<?= base_url('img/summer21/speaker.png') ?>" alt="安河内哲也氏">
+									</div>
+								</div> <!-- end of .speaker-box -->
 							</div>
 						</div> <!-- end of .row -->
 
-						<div class="speaker-box" id=speaker_box>
-							<h3 class="seminar-title">今の子どもたちに<br>必要な英語とは</h3>
-							<div class="speaker-info">
-								<div class="container-fluid">
-									<div class="row">
-										<div class="col-8 col-md-8">
-											<div class="text-center">
-												<h4>講師紹介</h4>
-											</div>
-											<p>
-												1967年福岡県北九州市生まれ。上智大学外国語学部英語学科卒。
-												東進ハイスクール・東進ビジネススクール英語科講師。
-												言語活動型英語授業を促進するために、スピーキングテスト、４技能試験の導入にむけて各所で活動中。
-												学校での講演の他、大手メーカーや金融機関でのグローバル化研修、教育委員会主催の教員研修事業の講師も務めている。
-											</p>
-										</div>
-									</div> <!-- end of .row -->
-								</div> <!-- end of .container-fluid -->
-
-								<img src="<?= base_url('img/summer21/speaker.png') ?>" alt="安河内哲也氏">
-							</div>
-						</div> <!-- end of .speaker-box -->
-
-						<div class="limit-num" id="limit_num">
-							<img src="<?= base_url('img/summer21/limit.png') ?>" alt="１塾２名限定">
-
-							<h4>定員</h4>
-
-							<div class="place-limit">
-								<span class="place-name">大阪</span>会場
-								<span class="first-come">先着</span>
-								<span class="number">150</span>
-								<span class="first-come">名</span>
-							</div>
-
-							<div class="place-limit">
-								<span class="place-name">京都</span>会場
-								<span class="first-come">先着</span>
-								<span class="number">&nbsp;80</span>
-								<span class="first-come">名</span>
-							</div>
-						</div>
 					</div> <!-- end of .container-fluid -->
 				</div> <!-- end of .detail-box -->
 
@@ -96,18 +99,25 @@
 						英語教育改革が進むなか、今の子どもたちに必要な英語とは何か。
 						安河内先生よりお話していただきます。
 					</p>
-					<dl class="event-data">
-						<dt class="event-date">開催日</dt>
-						<dd class="event-date">
-							大阪<span>６/１</span>(火)　京都<span>６/２</span>(水)
-						</dd>
 
-						<dt>時　間</dt>
-						<dd>
-							11:00～12:15　講演<br>
-							12:15～12:30　質疑応答
-						</dd>
-					</dl>
+					<div class="event-data-box">
+						<dl class="event-data">
+							<dt class="event-date">開催日</dt>
+							<dd class="event-date">
+								大阪<span class="ym">６/１</span>(火)　<br>
+								京都<span class="ym">６/２</span>(水)
+							</dd>
+
+							<dt>時　間</dt>
+							<dd>
+								<span class="overlapping">11:00～12:15　講演</span><br>
+								<span class="overlapping">12:15～12:30　質疑応答</span>
+							</dd>
+						</dl>
+
+						<img src="<?= base_url('img/summer21/speaker_sp.png') ?>" alt="安河内哲也氏">
+					</div>
+
 					<div class="speaker-box">
 						<div class="text-center">
 							<h4>講師紹介</h4>
@@ -117,8 +127,6 @@
 						東進ハイスクール・東進ビジネススクール英語科講師。
 						言語活動型英語授業を促進するために、スピーキングテスト、４技能試験の導入にむけて各所で活動中。
 						学校での講演の他、大手メーカーや金融機関でのグローバル化研修、教育委員会主催の教員研修事業の講師も務めている。
-
-						<img src="<?= base_url('img/summer21/speaker.png') ?>" alt="安河内哲也氏">
 					</div> <!-- end of .speaker-box -->
 
 					<div class="limit-num">
@@ -141,6 +149,32 @@
 						</div>
 					</div> <!-- end of .limit-num -->
 				</div> <!-- end of .detail-box-sp -->
+
+				<section class="for-sp">
+					<div class="row justify-content-center">
+						<p class="text-attention">当イベントは、事前のお申込みが必要となります。</p>
+					</div> <!-- end of .row -->
+
+					<div class="row justify-content-center mb-5">
+						<div class="col-12 col-md-4 text-right">
+							<?php echo form_button(array(
+								'name'		=> 'btn_apply_seminar',
+								'content'	=> 'セミナー&amp;テキスト展示のお申込み&nbsp;&nbsp;&nbsp;<i class="fas fa-chevron-right"></i>',
+								'class'		=> 'btn-summer21 btn-enabled',
+								'onclick'	=> 'location.href=\'' . site_url('summer21/apply_seminar/kansai') . '\''
+							)); ?>
+						</div>
+
+						<div class="col-12 col-md-4 text-left">
+							<?php echo form_button(array(
+								'name'		=> 'btn_apply_seminar',
+								'content'	=> '展示会のみのお申込み&nbsp;&nbsp;&nbsp;<i class="fas fa-chevron-right"></i>',
+								'class'		=> 'btn-summer21 btn-enabled mt-3 mt-md-0',
+								'onclick'	=> 'location.href=\'' . site_url('summer21/apply_exhibition/kansai') . '\''
+							)); ?>
+						</div>
+					</div> <!-- end of .row -->
+				</section>
 
 				<h1 class="title">会場一覧（参加無料&nbsp;/&nbsp;事前申込制）</h1>
 
@@ -312,6 +346,32 @@
 					</div> <!-- end of .limit-num -->
 				</div> <!-- end of .detail-box2-sp -->
 
+				<section class="for-sp">
+					<div class="row justify-content-center">
+						<p class="text-attention">当イベントは、事前のお申込みが必要となります。</p>
+					</div> <!-- end of .row -->
+
+					<div class="row justify-content-center mb-5">
+						<div class="col-12 col-md-4 text-right">
+							<?php echo form_button(array(
+								'name'		=> 'btn_apply_seminar',
+								'content'	=> 'セミナー&amp;テキスト展示のお申込み&nbsp;&nbsp;&nbsp;<i class="fas fa-chevron-right"></i>',
+								'class'		=> 'btn-summer21 btn-enabled',
+								'onclick'	=> 'location.href=\'' . site_url('summer21/apply_seminar/kansai') . '\''
+							)); ?>
+						</div>
+
+						<div class="col-12 col-md-4 text-left">
+							<?php echo form_button(array(
+								'name'		=> 'btn_apply_seminar',
+								'content'	=> '展示会のみのお申込み&nbsp;&nbsp;&nbsp;<i class="fas fa-chevron-right"></i>',
+								'class'		=> 'btn-summer21 btn-enabled mt-3 mt-md-0',
+								'onclick'	=> 'location.href=\'' . site_url('summer21/apply_exhibition/kansai') . '\''
+							)); ?>
+						</div>
+					</div> <!-- end of .row -->
+				</section>
+
 				<h1 class="title">会場一覧（参加無料&nbsp;/&nbsp;事前申込制）</h1>
 
 				<div class="row">
@@ -397,14 +457,24 @@
 						</p>
 
 						<div class="measures-list">
-							<img src="<?= base_url('img/summer21/measures1.png') ?>" alt="感染対策" class="narrow">
-							<img src="<?= base_url('img/summer21/measures2.png') ?>" alt="感染対策" class="narrow">
-							<img src="<?= base_url('img/summer21/measures3.png') ?>" alt="感染対策" class="narrow">
-							<img src="<?= base_url('img/summer21/measures4.png') ?>" alt="感染対策" class="narrow">
-							<img src="<?= base_url('img/summer21/measures5.png') ?>" alt="感染対策" class="wide">
+							<div class="image-wrapper narrow">
+								<img src="<?= base_url('img/summer21/measures1.png') ?>" alt="感染対策">
+							</div>
+							<div class="image-wrapper narrow">
+								<img src="<?= base_url('img/summer21/measures2.png') ?>" alt="感染対策">
+							</div>
+							<div class="image-wrapper narrow">
+								<img src="<?= base_url('img/summer21/measures3.png') ?>" alt="感染対策">
+							</div>
+							<div class="image-wrapper narrow">
+								<img src="<?= base_url('img/summer21/measures4.png') ?>" alt="感染対策">
+							</div>
+							<div class="image-wrapper wide">
+								<img src="<?= base_url('img/summer21/measures5.png') ?>" alt="感染対策">
+							</div>
 						</div>
 
-						<p class="description note">
+						<p class="description note for-pc">
 							※展示会開催前に、主催のスタッフを対象にＰＣＲ検査を行います。
 							各会場は、陰性が確認されたスタッフのみ参加いたします。
 						</p>
@@ -416,16 +486,28 @@
 				<div class="container">
 					<div class="lineup-content">
 						<img src="<?= base_url('img/summer21/new.png') ?>" alt="改訂・新版多数" class="new-label">
-						<p class="venue-arrow">TEXT LINE UP!</p>
-						<p class="lead">2021年は中学校教科書改訂年度！</p>
+						<div class="lineup-lead">
+							<p class="venue-arrow">TEXT LINE UP!</p>
+							<p class="lead">2021年は中学校教科書改訂年度！</p>
+						</div>
 						<h3>夏期講習用＆後期改訂・新刊テキストも併設展示します！</h3>
 
 						<div class="text-list">
-							<img src="<?= base_url('img/summer21/text_image1.png') ?>" alt="夏期テキスト">
-							<img src="<?= base_url('img/summer21/text_image1.png') ?>" alt="夏期テキスト">
-							<img src="<?= base_url('img/summer21/text_image1.png') ?>" alt="夏期テキスト">
-							<img src="<?= base_url('img/summer21/text_image1.png') ?>" alt="夏期テキスト">
-							<img src="<?= base_url('img/summer21/text_image1.png') ?>" alt="夏期テキスト">
+							<div class="image-wrapper">
+								<img src="<?= base_url('img/summer21/text_image1.png') ?>" alt="夏期テキスト">
+							</div>
+							<div class="image-wrapper">
+								<img src="<?= base_url('img/summer21/text_image1.png') ?>" alt="夏期テキスト">
+							</div>
+							<div class="image-wrapper">
+								<img src="<?= base_url('img/summer21/text_image1.png') ?>" alt="夏期テキスト">
+							</div>
+							<div class="image-wrapper">
+								<img src="<?= base_url('img/summer21/text_image1.png') ?>" alt="夏期テキスト">
+							</div>
+							<div class="image-wrapper">
+								<img src="<?= base_url('img/summer21/text_image1.png') ?>" alt="夏期テキスト">
+							</div>
 						</div>
 					</div> <!-- end of .lineup-content -->
 				</div> <!-- end of container -->
@@ -443,11 +525,17 @@
 					セミナー当日は、受付票を必ずご持参いただき、受付にお見せください。
 				</p>
 				<div class="apply-method-image">
-					<img src="<?= base_url('img/summer21/how_to_apply1.png') ?>" alt="フォーム入力">
+					<siv class="image-wrapper">
+						<img src="<?= base_url('img/summer21/how_to_apply1.png') ?>" alt="フォーム入力">
+					</siv>
 					<div class="arrow-triangle-right"></div>
-					<img src="<?= base_url('img/summer21/how_to_apply2.png') ?>" alt="受付票を保存">
+					<siv class="image-wrapper">
+						<img src="<?= base_url('img/summer21/how_to_apply2.png') ?>" alt="受付票を保存">
+					</siv>
 					<div class="arrow-triangle-right"></div>
-					<img src="<?= base_url('img/summer21/how_to_apply3.png') ?>" alt="受付票を当日持参">
+					<siv class="image-wrapper">
+						<img src="<?= base_url('img/summer21/how_to_apply3.png') ?>" alt="受付票を当日持参">
+					</siv>
 				</div>
 
 				<div class="row justify-content-center mb-5">
@@ -476,7 +564,5 @@
 	</div> <!-- end of .wrappewr -->
 
 	<?php $this->load->view('inc/_foot'); ?>
-
-	<script src="<?= base_url('js/front/summer21_kansai.js') ?>?var=<?= CACHES_CLEAR_VERSION ?>"></script>
 </body>
 </html>
