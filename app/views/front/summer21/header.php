c27885e49bb54484fdcbc1b5aa45f3c303a98acb
kamei <s-kamei@chuoh-kyouiku.co.jp>
2021-05-10 11:49:13 +0900

    中四国・九州中止

--- app/views/front/summer21/header.php
+++ app/views/front/summer21/header.php
@@ -2,5 +2,25 @@
 	<div class="container">
 		<img src="<?= base_url('img/summer21/header_pc.png') ?>" alt="CHUOH TEXT COLLECTION" class="for-pc">
 		<img src="<?= base_url('img/summer21/header_sp.png') ?>" alt="CHUOH TEXT COLLECTION" class="for-sp">
+
+		<div class="discontinuance">
+			<h2>当イベントは中止となりました</h2>
+		</div>
 	</div> <!-- end of container -->
 </header>
+
+<div class="container">
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="apology-box">
+				<h3>プレミアムセミナー開催中止の決定に関するお詫び</h3>
+				<p>
+					新型コロナウィルス感染症の感染急拡大に伴う緊急事態宣言発令のため、
+					来場者および関係者の健康や安全面を第一に考えました結果、
+					5月25日に開催を予定していた当イベントを中止することを決定しました。<br>
+					皆様のご理解をいただきます様、何卒よろしくお願い申し上げます。
+				</p>
+			</div> <!-- end of .apology-box -->
+		</div>
+	</div> <!-- end of .row -->
+</div> <!-- end of container -->
--- app/views/front/summer21/header_chushikoku.php
+++ app/views/front/summer21/header_chushikoku.php
@@ -1,5 +1,24 @@
 <header>
 	<div class="container">
 		<img src="<?= base_url('img/summer21/header_pc_chushikoku.png') ?>" alt="CHUOH TEXT COLLECTION">
+		<div class="discontinuance">
+			<h2>当イベントは中止となりました</h2>
+		</div>
 	</div> <!-- end of container -->
 </header>
+
+<div class="container">
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="apology-box">
+				<h3>夏期展示会開催中止の決定に関するお詫び</h3>
+				<p>
+					緊急事態宣言・まん延防止等重点措置の発令および全国的な感染状況を鑑みて、
+					来場者および関係者の健康や安全面を第一に考えました結果、
+					5月25日～5月31日に開催を予定していた当イベントを中止することを決定しました。<br>
+					皆様のご理解をいただきます様、何卒よろしくお願い申し上げます。
+				</p>
+			</div> <!-- end of .apology-box -->
+		</div>
+	</div> <!-- end of .row -->
+</div> <!-- end of container -->
