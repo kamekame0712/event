adb2cd5f37491d91df971fc3fd12fd02fa8d1bd4
s-kamei <s-kamei@chuoh-kyouiku.co.jp>
2021-09-13 12:20:46 +0900

    デザイン修正

--- app/views/front/winter21/header_form.php
+++ app/views/front/winter21/header_form.php
@@ -1,6 +1,6 @@
-<header>
+<header class="py-0">
 	<div class="container">
-		<img src="<?= base_url('img/winter21/form_top_pc.png') ?>" alt="冬季展示会" class="for-pc">
+		<img src="<?= base_url('img/winter21/form_top_pc.png') ?>" alt="冬季展示会" class="for-pc form-top-pc">
 		<img src="<?= base_url('img/winter21/form_top_sp.png') ?>" alt="冬季展示会" class="for-sp">
 	</div> <!-- end of container -->
 </header>
